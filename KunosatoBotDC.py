be8d2822b880e43af9c9a304a2817903f6ad52ae
doomedducks21-bit <doomedducks21@gmail.com>
2026-04-25 13:53:09 +0100

    Update KunosatoBotDC.py

--- KunosatoBotDC.py
+++ KunosatoBotDC.py
@@ -35,13 +35,13 @@ async def help_me(ctx):
 @bot.hybrid_command(name="the_butcher", description="The butcher revives")
 @discord.app_commands.allowed_installs(guilds=True, users=True)
 @discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-async def the_butcher(ctx):
+async def the_butcher(ctx, hidden : bool):
     link = "https://cdn.discordapp.com/attachments/1473382001573625906/1489007834606731365/caption.gif?ex=69ed2c9c&is=69ebdb1c&hm=1ce9a1e11068b7dfd6a107b14af1a51192a7dee6a2ac13d1e655c00e415d2bbc&"
-    await ctx.send(f"The Butcher: {link}", ephemeral=True)
+    await ctx.send(f"The Butcher: {link}", ephemeral=hidden)
 @bot.hybrid_command(name="butcher_gallery",description="all de butchers")
 @discord.app_commands.allowed_installs(guilds=True,users=True)
 @discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-async def butcher_g(ctx):
+async def butcher_g(ctx, hidden : bool):
     glink = "https://cdn.discordapp.com/attachments/1473382001573625906/1489007834606731365/caption.gif?ex=69ed2c9c&is=69ebdb1c&hm=1ce9a1e11068b7dfd6a107b14af1a51192a7dee6a2ac13d1e655c00e415d2bbc&"
     embed1 = discord.Embed(url=glink, title="Butcher Gallery")
     embed1.set_image(url="https://cdn.discordapp.com/attachments/1473382001573625906/1489007834606731365/caption.gif?ex=69ed2c9c&is=69ebdb1c&hm=1ce9a1e11068b7dfd6a107b14af1a51192a7dee6a2ac13d1e655c00e415d2bbc&")
@@ -51,11 +51,11 @@ async def butcher_g(ctx):
     embed3.set_image(url="https://cdn.discordapp.com/attachments/1473382001573625906/1495068549872619571/caption.gif?ex=69eccf97&is=69eb7e17&hm=26d39fbbfe2b2a147957401ca4899d2d4f4fdbc7fb3a04d5ccfd72726f891413&")
     embed4 = discord.Embed(url=glink, title="Butcher Gallery")
     embed4.set_image(url="https://cdn.discordapp.com/attachments/1473382001573625906/1479984517153296476/caption.gif?ex=69ed4e7d&is=69ebfcfd&hm=33cfd25d7b6c5e7ec123be930d0c4d7c720dd4faceca2ec936700af871ef4afc&")
-    await ctx.send(embeds=[embed1,embed2,embed3,embed4],ephemeral=True)
+    await ctx.send(embeds=[embed1,embed2,embed3,embed4],ephemeral=hidden)
 @bot.hybrid_command(name="chefs_kiss", description="meal")
 @discord.app_commands.allowed_installs(guilds=True,users=True)
 @discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-async def chef_g(ctx):
+async def chef_g(ctx, hidden : bool):
     glink = "https://cdn.discordapp.com/attachments/1473382001573625906/1489007834606731365/caption.gif?ex=69ed2c9c&is=69ebdb1c&hm=1ce9a1e11068b7dfd6a107b14af1a51192a7dee6a2ac13d1e655c00e415d2bbc&"
     embed1 = discord.Embed(url=glink, title="CLICK ON A GIF THEN PRESS THE ARROWS TO GET MORE, THERE IS 10 IN THIS SECTION")
     embed1.set_image(url="https://cdn.discordapp.com/attachments/1046810298075582518/1473369102549516389/caption.gif?ex=69eda127&is=69ec4fa7&hm=b5f5c4e34ada4b6dc8b28ea2cc550b9b617a9e48e445265c8911fa01aebb0a45")
@@ -77,12 +77,12 @@ async def chef_g(ctx):
     embed9.set_image(url="https://cdn.discordapp.com/attachments/1046810298075582518/1473835200869044357/caption.gif?ex=69ed58fe&is=69ec077e&hm=f4bc9797ca9b48a57e1a50344b8bcbbde56f4f8cce4b5b363f7416c22972c117&")
     embed10 = discord.Embed(url=glink, title="Meal Gallery")
     embed10.set_image(url="https://media.discordapp.net/attachments/1104321989100974163/1495188822898638928/IMG_3943.gif?ex=69ede85a&is=69ec96da&hm=5543273932ff42b57d8dee42bfed1bc144c2aaafa0a5f7b6845e18197b930e81&=")
-    await ctx.send(f"CLICK ON A GIF THEN PRESS THE ARROWS TO GET MORE, THERE IS 10 IN THIS SECTION",embeds=[embed1,embed2,embed3,embed4,embed5,embed6,embed7,embed8,embed9,embed10],ephemeral=True)
+    await ctx.send(f"CLICK ON A GIF THEN PRESS THE ARROWS TO GET MORE, THERE IS 10 IN THIS SECTION",embeds=[embed1,embed2,embed3,embed4,embed5,embed6,embed7,embed8,embed9,embed10],ephemeral=hidden)
 
 @bot.hybrid_command(name="roblox_fun_pack",description="welcome to roblox friend")
 @discord.app_commands.allowed_installs(guilds=True,users=True)
 @discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-async def roblox_g(ctx):
+async def roblox_g(ctx, hidden : bool):
     glink = "https://cdn.discordapp.com/attachments/1473382001573625906/1489007834606731365/caption.gif?ex=69ed2c9c&is=69ebdb1c&hm=1ce9a1e11068b7dfd6a107b14af1a51192a7dee6a2ac13d1e655c00e415d2bbc&"
     embed1 = discord.Embed(url=glink, title="Roblox create imagine explore")
     embed1.set_image(url="https://cdn.discordapp.com/attachments/1157023650457862247/1157029193385783379/ezgif-5-b998c47733.gif?ex=69edb828&is=69ec66a8&hm=48f3e9a64feec97559bc39dd63391d0d01384173f8041f1a57bef9b3282e438f&")
@@ -92,11 +92,11 @@ async def roblox_g(ctx):
     embed3.set_image(url="https://cdn.discordapp.com/attachments/1473382001573625906/1490324209522704424/caption.gif?ex=69ee0214&is=69ecb094&hm=f5f8ad3f368271ea56be41114d674da4201778f00b352676c00233b5eb73ad74&")
     embed4 = discord.Embed(url=glink, title="Roblox create imagine explore")
     embed4.set_image(url="https://media.discordapp.net/attachments/1006632209232437272/1280565098594701415/ezgif-6-8e22c3abd1.gif?ex=69ed946f&is=69ec42ef&hm=8e75dfcfa8452b623de5c0d08f1ef27627631a41c06de84141710f00672b30be&")
-    await ctx.send(embeds=[embed1,embed2,embed3,embed4],ephemeral=True)
+    await ctx.send(embeds=[embed1,embed2,embed3,embed4],ephemeral=hidden)
 @bot.hybrid_command(name="methods",description="Is that you... Ibrahim?")
 @discord.app_commands.allowed_installs(guilds=True,users=True)
 @discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-async def method_g(ctx):
+async def method_g(ctx, hidden : bool):
     glink = "https://cdn.discordapp.com/attachments/1473382001573625906/1489007834606731365/caption.gif?ex=69ed2c9c&is=69ebdb1c&hm=1ce9a1e11068b7dfd6a107b14af1a51192a7dee6a2ac13d1e655c00e415d2bbc&"
     embed1 = discord.Embed(url=glink, title="Ibrahim's methods!")
     embed1.set_image(url="https://media.discordapp.net/attachments/934630543637762109/965466390200975360/caption.gif?ex=69ed9224&is=69ec40a4&hm=45a7a2eee30a43b75058c2fcbfff5ad3c17be6543b8f96b6ce81d0b6c2371bee&")
@@ -106,6 +106,6 @@ async def method_g(ctx):
     embed3.set_image(url="https://cdn.discordapp.com/attachments/860539149046906920/1274456254554574899/ezgif.com-crop.gif?ex=69edc4a1&is=69ec7321&hm=10bf6f958b346266b9552d5a759c16abb820c74d3a4189daed995dd0b4a97eb4&")
     embed4 = discord.Embed(url=glink, title="Roblox create imagine explore")
     embed4.set_image(url="https://media.discordapp.net/attachments/893508396718911511/1152971856735903826/nBOuHUOW.gif?ex=69ed75f7&is=69ec2477&hm=06fdedbb4fe243e9fed4c92228f904ac46a437efa1baa2d1faed7e53e64ba63e&")
-    await ctx.send(embeds=[embed1,embed2,embed3,embed4],ephemeral=True)
+    await ctx.send(embeds=[embed1,embed2,embed3,embed4],ephemeral=hidden)
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
