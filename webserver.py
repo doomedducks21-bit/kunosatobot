74b192c33d06e9ad4fad95a27bdf8d84caac97e8
doomedducks21-bit <doomedducks21@gmail.com>
2026-04-25 12:39:58 +0100

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -9,11 +9,10 @@ def home():
     return "ok"
 
 def run():
-    
+   
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
-
     t = Thread(target=run)
-    t.start()
+    t.start() 
